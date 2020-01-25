ffa01f08956e13b66bfca0bcd70d155bf34335fe
Philip Akpeki <philipakpeki@gmail.com>
2020-01-25 09:14:46 +0100

    Initial commit

---
+++ src/main/java/com/devphilip/devpbuddy/DevpbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.devphilip.devpbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpbuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevpbuddyApplication.class, args);
+    }
+
+}
