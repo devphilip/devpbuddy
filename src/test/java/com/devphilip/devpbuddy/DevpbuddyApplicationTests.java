399c7bb00b0a4c9246d33e13209da35efc9635e9
Philip Akpeki <philipakpeki@gmail.com>
2020-01-26 04:36:04 +0100

    Setup I18N feature to the app

--- src/test/java/com/devphilip/devpbuddy/DevpbuddyApplicationTests.java
+++ src/test/java/com/devphilip/devpbuddy/DevpbuddyApplicationTests.java
@@ -1,13 +1,25 @@
 package com.devphilip.devpbuddy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devphilip.devpbuddy.web.i18n.I18NService;
 
 @SpringBootTest
 class DevpbuddyApplicationTests {
-
+	
+	@Autowired
+	private I18NService I18NService;
+	
 	@Test
-	void contextLoads() {
+	public void testMessageByLocalService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = I18NService.getMessage(messageId);
+		assertEquals(expectedResult, actual, "The actual and expected String don't match");
 	}
 
 }
